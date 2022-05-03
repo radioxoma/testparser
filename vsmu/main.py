d1e61e461bc083e61bb3a01621a7df5c6862f1fb
Eugene Dvoretsky <radioxoma@gmail.com>
2022-05-04 02:37:41 +0300

    Sorting is optional

--- vsmu/main.py
+++ vsmu/main.py
@@ -482,12 +482,13 @@ def main():
     parser.add_argument('target', choices=('evsmu', 'do', 'mytestx', 'raw', 'raw2'), help="Parse e-vsmu.by, do.vsmu.by HTML; mytextx, raw tests")
     parser.add_argument("input", nargs="+", help="An *.htm file (or files) for parsing. Multiple files will be concatenated.")
     parser.add_argument("--na", action='store_false', help="Do not raise an exception if page doesn't have question answers. Normally, if there is nonequal count of variants and answers, program will quit.")
-    parser.add_argument("-u", "--unify", action='store_true', help="Remove duplicated tests. Case-sensitive. Use it if joining multiple HTML files.")
+    parser.add_argument("-u", "--unify", action='store_true', help="Remove duplicated tests. Case-sensitive. Use it when joining multiple HTML files.")
     parser.add_argument("-d", "--duplicates", action='store_true', help="Print duplicates.")
     parser.add_argument("-p", action='store_true', help="Print parsed tests in STDOUT.")
+    parser.add_argument("-s", "--sort", action='store_true', help="Sort tests.")
     parser.add_argument("--to-mytestx", help="Save formatted text into *.txt Windows-1251 encoded file. Fine for printing (file is human-readable) or importing in http://mytest.klyaksa.net")
     parser.add_argument("--to-anki", help="Save as tab-formatted text file for import in Anki cards http://ankisrs.net")
-    parser.add_argument("--to-crib", help="Save as shortened cheat sheet text.")
+    parser.add_argument("--to-crib", help="Save as sorted shortened cheat sheet text.")
     args = parser.parse_args()
     # Define test source & parse to Question class instances
     tests = list()
@@ -517,7 +518,8 @@ def main():
         print('\n'.join([k.to_string() for k in dup]))
 
     # Sorting important for crib shortener!
-    tests.sort(key=lambda q: q.to_string().lower())
+    if args.sort or args.to_crib:
+        tests.sort(key=lambda q: q.to_string().lower())
 
     # Output
     if args.p:
